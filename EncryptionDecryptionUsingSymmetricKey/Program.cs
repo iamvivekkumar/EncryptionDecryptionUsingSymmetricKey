265d7882547fad4361b16c71d88c51a9e417a9e3
vivek kumar <vivek.kumar@shoregrp.com>
2019-03-09 01:10:54 +0530

    Initial Commit

---
+++ EncryptionDecryptionUsingSymmetricKey/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");
+            //var key = Console.ReadLine();
+
+            Console.WriteLine("Please enter a string for encryption");
+            var str = Console.ReadLine();
+            var encryptedString = AesOperation.EncryptString(key, str);
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = AesOperation.DecryptString(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
